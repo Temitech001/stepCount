38149f56f4b083267ed0a1eb35b59901d7040a0d
samuel <olaoluwasamuel119@gmail.com>
2023-09-22 18:50:16 -0400

    new

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@
 
 // export default App;
 
-import { useState } from 'react';
+iimport { useState } from 'react';
 
 const messages = [
   'Learn React',
@@ -95,27 +95,34 @@ function App() {
         &times;
       </button>
       {isOpen && (
-        <div className="steps w-full md:w-[80%] mx-auto my-4">
-          <div className="numbers">
-            <div className={step >= 1 ? 'active' : ''}>1</div>
-            <div className={step >= 2 ? 'active' : ''}>2</div>
-            <div className={step >= 3 ? 'active' : ''}>3</div>
-            <div className={step >= 4 ? 'active' : ''}>4</div>
-            <div className={step >= 5 ? 'active' : ''}>5</div>
+        <div className="steps p-4">
+          <div className="numbers flex justify-center space-x-2">
+            {[1, 2, 3, 4, 5].map((number) => (
+              <div
+                key={number}
+                className={`${
+                  step >= number ? 'bg-blue-500' : 'bg-gray-300'
+                } h-6 w-6 rounded-full flex items-center justify-center text-white`}
+              >
+                {number}
+              </div>
+            ))}
           </div>
-          <p className="">
+          <p className="text-center mt-4">
             step {step}: {messages[step - 1]}
           </p>
-          <div className="buttons">
+          <div className="buttons flex justify-center mt-4">
             <button
               onClick={handlePrevious}
               style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              className="px-4 py-2 mr-4 rounded"
             >
               Previous
             </button>
             <button
               onClick={handleNext}
               style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              className="px-4 py-2 rounded"
             >
               Next
             </button>
